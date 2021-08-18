b99801f7a78fc5bb473924ff403a206a2374da9e
MarleySrd <m.sorhaindo@codeur.online>
2021-08-18 15:50:01 +0200

    incrustation du js pour le menu

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var frameIndex = 0;
+      showFrames(frameIndex);
+
+      function currentFrame(n) {
+          showFrames(frameIndex = n);
+      }
+
+      function showFrames(n) {
+          var i;
+          var frames = document.getElementsByClassName("frame");
+          var dots = document.getElementsByClassName("buttom-article");
+
+          for (i = 0; i < frames.length; i++)
+          {
+              frames[i].style.display = "none";
+          }
+          for (i = 0; i < dots.length; i++)
+          {
+              dots[i].className = dots[i].className.replace(" active", "");
+          }
+          frames[frameIndex].style.display = "block";
+          dots[frameIndex].className += " active";
+      }

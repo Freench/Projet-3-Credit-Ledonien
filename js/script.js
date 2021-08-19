45fea15b1d83c951db66f8f87215def0937b417d
MarleySrd <m.sorhaindo@codeur.online>
2021-08-19 15:27:00 +0200

    position, font-size and font-height

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 var frameIndex = 0;
-      showFrames(frameIndex);
+showFrames(frameIndex);
 
-      function currentFrame(n) {
-          showFrames(frameIndex = n);
-      }
+function currentFrame(n) {
+    showFrames(frameIndex = n);
+}
 
-      function showFrames(n) {
-          var i;
-          var frames = document.getElementsByClassName("frame");
-          var dots = document.getElementsByClassName("buttom-article");
+function showFrames(n) {
+    var i;
+    var frames = document.getElementsByClassName("frame");
+    var dots = document.getElementsByClassName("buttom-article");
 
-          for (i = 0; i < frames.length; i++)
-          {
-              frames[i].style.display = "none";
-          }
-          for (i = 0; i < dots.length; i++)
-          {
-              dots[i].className = dots[i].className.replace(" active", "");
-          }
-          frames[frameIndex].style.display = "block";
-          dots[frameIndex].className += " active";
-      }
+    for (i = 0; i < frames.length; i++) {
+        frames[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    frames[frameIndex].style.display = "block";
+    dots[frameIndex].className += " active";
+}

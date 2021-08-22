b39c1bf10502b76cc0971d498b6197b3b3bc0ec7
Freench <f.pernot@codeur.online>
2021-08-22 23:27:50 +0200

    intégration php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Crédit Lédonien - Le crédit responsable</title>
+    <meta name="description" content="Essayez notre simulateur de crédit, nous avons vos solution de crédit et nous vous accompagnons vers une solution responsable.">
+</head>
+
+<body class="flex">
+    <!-- <header>
+        <div class="banner">
+            <img src="images/logo_top.png" alt="Logo du Crédit Lédonien" id="logo">
+            <div class="top-warning">
+                <p>Un crédit vous engage et doit être remboursé.</p>
+                <p>Vérifiez vos capacités de remboursement avant de vous engager.</p>
+            </div>
+        </div>
+    </header> -->
+    <div class="header-main">
+        header
+    </div>
+    <div class="pret-main">
+        pret
+    </div>
+    <div class="simulateur-main">
+        <?php include('html/simulateur.php')
+        ?>
+    </div>
+    <div class="article1-main">
+        article-1
+    </div>
+    <div class="decouverte-main">
+        decouverte
+    </div>
+    <div class="photo-main">
+        photo
+    </div>
+    <div class="article2-main">
+        article2
+    </div>
+    <div class="footer-main">
+        footer
+    </div>
+</body>
+
+</html>

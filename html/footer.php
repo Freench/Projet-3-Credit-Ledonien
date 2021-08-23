102083a6a617f14fb6a44d533b1014cba06c6024
DylanCharton <d.charton@codeur.online>
2021-08-23 14:38:20 +0200

    Prépa mergation

---
+++ html/footer.php
@@ -0,0 +1,37 @@
+
+    <!-- FOOTER -->
+    
+        <!-- FIRST PART OF THE FOOTER -->
+        <div class="topfooter">
+            <nav>
+                <ul class ="sitemap">
+                    <li><a href="" class="sitemaplink">Accueil</a></li>
+                    <li><a href="" class="sitemaplink">Qui sommes-nous?</a></li>
+                    <li><a href="" class="sitemaplink">Nos Offres</a></li>
+                    <li><a href="" class="sitemaplink">Contactez-nous</a></li>
+                </ul>
+            </nav>
+        </div>
+        <!-- SECOND PART OF THE FOOTER -->
+        <div class="botfooter">
+            <div class="leftbotfooter">
+                <div class="logofooter"><img src="images/logowhite.png" alt="Logo de l'entreprise"></div>
+                <p class="loanwarning">Un crédit vous engage et doit être remboursé. <br/> Vérifiez vos capacités de remboursement avant de vous engager.</p>
+            </div>
+            <div class="centerbotfooter">
+                <div class="infosfooter">
+                    <p>1 place de la République</p>
+                    <p>39000 Lons le Saunier</p>
+                    <p>Tél: + 33 (0) 3 84 12 56 89</p>
+                    <p>contact@lecreditledonien.fr</p>
+                </div>                
+            </div>
+            <div class="rightbotfooter">
+                <a href="" class="legallink">Mentions légales</a>
+                <a href="" class="legallink">Politique de confidentialité</a>
+                <p>Une réalisation <a href="https://promo-93.codeur.online/blog/" target="_blank" rel="noopener" class="credit">ACS Lons le Saunier</a></p>
+            </div>
+        </div>
+
+    
+     

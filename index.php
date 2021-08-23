0b41f7e2af7b1146c23d0879f8ecec60aa8567d2
DylanCharton <d.charton@codeur.online>
2021-08-23 14:28:52 +0200

    effacé une ligne

--- index.php
+++ index.php
@@ -10,17 +10,9 @@
 </head>
 
 <body class="flex">
-    <!-- <header>
-        <div class="banner">
-            <img src="images/logo_top.png" alt="Logo du Crédit Lédonien" id="logo">
-            <div class="top-warning">
-                <p>Un crédit vous engage et doit être remboursé.</p>
-                <p>Vérifiez vos capacités de remboursement avant de vous engager.</p>
-            </div>
-        </div>
-    </header> -->
+    
     <div class="header-main">
-        header
+        
     </div>
     <div class="pret-main">
         pret
